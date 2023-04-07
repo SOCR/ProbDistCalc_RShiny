8b574520374092c14d3ca7c5352c49720b9b19a0
Enming Xu <enming.xu.15@gmail.com>
2023-04-07 03:00:47 -0400

    Bivariate Normal Distribution done

--- plotlyFunctions/BivariateNormal3D.R
+++ plotlyFunctions/BivariateNormal3D.R
@@ -25,10 +25,11 @@ plotlyBivariateNormal3DDistribution <- function(plotrange, input, distType, prob
                 newy[index] = NA
             }
         }
-        prob = exp(-(as.numeric(input$BivaV2)*(probrange[2]-as.numeric(input$BivaM1))^2+as.numeric(input$BivaV1)*(probrange[2]-as.numeric(input$BivaM2))^2-2*as.numeric(input$BivaCov)*(probrange[2]-as.numeric(input$BivaM1))*(probrange[2]-as.numeric(input$BivaM2)))/(2*(as.numeric(input$BivaV1)*as.numeric(input$BivaV2)-as.numeric(input$BivaCov)^2)))/(2*pi*sqrt(as.numeric(input$BivaV1)*as.numeric(input$BivaV2)-as.numeric(input$BivaCov)^2)) -
-            exp(-(as.numeric(input$BivaV2)*(probrange[1]-as.numeric(input$BivaM1))^2+as.numeric(input$BivaV1)*(probrange[1]-as.numeric(input$BivaM2))^2-2*as.numeric(input$BivaCov)*(probrange[1]-as.numeric(input$BivaM1))*(probrange[1]-as.numeric(input$BivaM2)))/(2*(as.numeric(input$BivaV1)*as.numeric(input$BivaV2)-as.numeric(input$BivaCov)^2)))/(2*pi*sqrt(as.numeric(input$BivaV1)*as.numeric(input$BivaV2)-as.numeric(input$BivaCov)^2))
-
-        fig <- fig %>% add_surface()
+        prob <- pmnorm(cbind(as.numeric(probrange[2]),as.numeric(probrange[2])), c(as.numeric(input$BivaM1), as.numeric(input$BivaM2)), matrix(c(as.numeric(input$BivaV1), as.numeric(input$BivaCov), as.numeric(input$BivaCov), as.numeric(input$BivaV2)), 2))
+            - pmnorm(cbind(as.numeric(probrange[1]),as.numeric(probrange[1])), c(as.numeric(input$BivaM1), as.numeric(input$BivaM2)), matrix(c(as.numeric(input$BivaV1), as.numeric(input$BivaCov), as.numeric(input$BivaCov), as.numeric(input$BivaV2)), 2))
+            
+        fig <- fig %>% add_surface(x = x1, y = x2, z = f10, name = paste("Probability = ", prob, sep = ""),
+                hoverinfo = "name")
         fig <- fig %>%
             plotly::layout(title = paste(distributions[10], " - ", graphtype, sep = ""),
                     hovermode = "x", hoverlabel = list(namelength = 100), 
